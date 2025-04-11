eed67a187231147eeb4aee6fab17958269d09a35
Joong Seok Hwang <93852829+jung-seok-hwang@users.noreply.github.com>
2025-04-11 18:44:34 +0900

    🎉init : Init project

---
+++ presentation/src/main/java/com/yju/presentation/di/AppApplication.kt
@@ -0,0 +1,13 @@
+package com.yju.presentation.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class AppApplication :Application(){
+    override fun onCreate() {
+        super.onCreate()
+//        Timber.plant(Timber.DebugTree())
+    }
+}
